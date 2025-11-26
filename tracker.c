5291843b6c2e08a6c47cab54c161c2b67c235470
Kommanapalli Aakash Chandra <kommanapalliaakashchandra@gnmail.com>
2025-11-26 17:43:36 +0530

    fix: added constructor to clear log file on startup

--- tracker.c
+++ tracker.c
@@ -3,10 +3,17 @@
 #include <stdlib.h>
 #include <dlfcn.h>
 #include <unistd.h>
+#include <fcntl.h>  // <--- Added this
 
 // Pointers to the real memory functions
 static void *(*real_malloc)(size_t) = NULL;
 static void (*real_free)(void *) = NULL;
+
+// <--- Added this function: Clears the log file when the program starts
+void __attribute__((constructor)) init() {
+    FILE *fp = fopen("memory.log", "w");
+    if (fp) fclose(fp);
+}
 
 // Hook for malloc
 void *malloc(size_t size) {
@@ -30,9 +37,9 @@ void free(void *p) {
     if (!real_free) {
         real_free = dlsym(RTLD_NEXT, "free");
     }
-
+    
     real_free(p);
-
+    
     // We log '0' size to represent a free event for now
     FILE *fp = fopen("memory.log", "a");
     if (fp) {
